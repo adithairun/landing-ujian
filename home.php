32e4a00dc69cf81c6beae37d295c59e1e8a3e152
Aditya Nugraha <adityanugraha795@gmail.com>
2022-03-12 20:49:23 +0800

    up

---
+++ home.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="shortcut icon" href="img/favicon.ico">
+
+  <title>COMPUTER BASED TEST SMAN 1 GORONTALO</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Merriweather:300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template -->
+  <link href="css/coming-soon.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <div class="overlay"></div>
+<video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop"> 
+    <source src="mp4/bg.mp4" type="video/mp4"> 
+     
+  </video>
+
+  <div class="masthead">
+    <div class="masthead-bg"></div>
+    <div class="container h-100">
+      <div class="row h-100">
+        <div class="col-12 my-auto">
+          <div class="masthead-content text-white py-5 py-md-0">
+            <h1 class="mb-3">CBT SMAN 1 GORONTALO</h1>
+            <p class="mb-5">Silahkan Pilih Sesuai Kelas Kalian </p>
+            <div class="input-group input-group-newsletter">
+              
+          <!--    <div class="input-group-append">
+                <button onclick="myFunction()" class="btn btn-primary btn-lg">KELAS XII IPA</button>
+              
+			   <br>
+                <button onclick="myFunction()" class="btn btn-success btn-lg" >KELAS XII BAHASA <br> DAN XII IPS</a>
+              </div> -->
+			  
+			  <div class="input-group-append">
+        <!--   <a href="kelas-x" class="btn btn-success btn-lg">KELAS X</a> -->
+          <button onclick="myFunction()" class="btn btn-success btn-lg">KELAS X</button> 
+              
+			   
+         <!--       <a href="kelas-xi" class="btn btn-primary btn-lg" >KELAS XI</a> -->
+				
+				 <button onclick="myFunction()" class="btn btn-primary btn-lg">KELAS XI</button> 
+                
+				
+              <!--  <a href="kelas-xi" class="btn btn-warning btn-lg" >KELAS XII</a> -->
+           <!--     <button onclick="myFunction()" class="btn btn-warning btn-lg" >KELAS XII</a> -->			<a href="kelas-xii" class="btn btn-warning btn-lg" >KELAS XII</a> 
+              </div>
+			  
+			  
+			  <script>
+function myFunction() {
+  alert("Belum Waktunya Ujian :)");
+}
+</script>
+			   <div class="row ">
+<div class="col-xl-8 col-md-4 col-sm-4 col-auto my-md-0 mt-5 order-sm-1 order-3 align-self-end">
+                         <h4 class="social text-muted mb-0 pb-0 bold-text"> <span class="mx-2"><i class="fa fa-facebook" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-linkedin-square" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-twitter" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-instagram" aria-hidden="true"></i></span> </p><small class="rights"><font color="white"><span>Support By</span> <br><a href="https://tim-it.sma1gorontalo.sch.id">TIM IT SMAN 1 GORONTALO</a></font></small>
+                     </div>
+					 </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="social-icons">
+    <ul class="list-unstyled text-center mb-0">
+      <li class="list-unstyled-item">
+        <a href="#">
+          <i class="fab fa-youtube"></i>
+        </a>
+      </li>
+      <li class="list-unstyled-item">
+        <a href="https://fb.com/">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+      </li>
+      <li class="list-unstyled-item">
+        <a href="https://instagram.com/hubmas.smansagorontalo">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/coming-soon.min.js"></script>
+
+</body>
+
+</html>
